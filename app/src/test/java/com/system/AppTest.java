02905346393255baabe3a2807220cb1f3f5209a3
叶振南 <YEZHENNAN220@pingan.com.cn>
2018-11-02 11:24:57 +0800

    通过属性配置文件给对象赋值

--- app/src/test/java/com/system/AppTest.java
+++ app/src/test/java/com/system/AppTest.java
@@ -1,20 +1,28 @@
 package com.system;
 
-import static org.junit.Assert.assertTrue;
 
+import com.system.business.domain.AppInfo;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+
+    @Autowired
+    AppInfo appInfo;
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        System.out.println(appInfo);
     }
 }
