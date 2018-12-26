68237be59c8ae8234d29f5dd4e1cc78ea4b87a84
叶振南 <YEZHENNAN220@pingan.com.cn>
2018-12-26 14:55:50 +0800

    修改服务器端口

--- app/src/main/java/com/system/configuration/SwaggerConfiguration.java
+++ app/src/main/java/com/system/configuration/SwaggerConfiguration.java
@@ -12,9 +12,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Swagger2 API文档工具配置文件，必须有
- * */
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
@@ -34,13 +37,23 @@ public class SwaggerConfiguration implements ApplicationListener<EmbeddedServlet
                 //自定义信息可按需求填写
                 .title("Spring Boot中使用Swagger构建RESTful APIs")
                 .description("测试集成")
-                .termsOfServiceUrl("http://127.0.0.1:8080/")
+                .termsOfServiceUrl(getAddress() + ":8081/")
                 .version("1.0")
                 .build();
     }
 
     @Override
     public void onApplicationEvent(EmbeddedServletContainerInitializedEvent embeddedServletContainerInitializedEvent) {
-        System.err.println("swagger访问地址:http://" + "127.0.0.1" + ":" + "8080" + "/swagger-ui.html");
+        System.err.println("swagger访问地址:http://" + getAddress() + ":" + "8081" + "/swagger-ui.html");
+    }
+
+    private String getAddress() {
+        String hostAddress;
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            hostAddress = "127.0.0.1";
+        }
+        return hostAddress;
     }
 }
